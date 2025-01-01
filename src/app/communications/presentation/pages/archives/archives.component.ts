c2209e52f09e520168e080f1f1cd0600398bb35f
Gustavo Flores <gustavfs2003@gmail.com>
2024-12-31 22:56:04 -0400

    correcion seccion de archivos

--- src/app/communications/presentation/pages/archives/archives.component.ts
+++ src/app/communications/presentation/pages/archives/archives.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ArchiveDialogComponent } from './archive-dialog/archive-dialog.component';
+import { ArchiveService } from '../../services/archive.service';
 
 @Component({
   selector: 'app-archives',
@@ -11,27 +14,52 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ArchivesComponent { 
-  folders = [
-    { name: 'Documentos' },
-    { name: 'Imágenes' },
-    { name: 'Videos' },
-    { name: 'Proyectos' },
-    { name: 'Descargas' },
-    { name: 'Música' },
-    { name: 'Presentaciones' },
-    { name: 'Reportes' },
-  ];
-  selectedFolder: number | null = null;
+  folders = signal<any[]>([]);
 
-  selectFolder(index: number): void {
-    this.selectedFolder = this.selectedFolder === index ? null : index;
+  constructor(private dialog: MatDialog,private archiveService: ArchiveService) {
+    this.loadFolders();
+
   }
 
-  deleteFolder(index: number, event: MouseEvent): void {
-    event.stopPropagation(); // Evita que el evento cierre la selección
-    this.folders.splice(index, 1); // Elimina la carpeta
-    if (this.selectedFolder === index) {
-      this.selectedFolder = null; // Deselecciona la carpeta eliminada
-    }
+  loadFolders() {
+    this.archiveService.getFolders().subscribe({
+      next: (data) => {
+        this.folders.set(data);
+      },
+      error: (err) => {
+        console.error('Error al cargar carpetas:', err);
+      },
+    });
+  }
+
+  openCreateFolderDialog() {
+    const dialogRef = this.dialog.open(ArchiveDialogComponent);
+
+    dialogRef.afterClosed().subscribe((folderName) => {
+      if (folderName) {
+        this.archiveService.createFolder(folderName).subscribe({
+          next: (newFolder) => {
+            this.folders.update((values)=>[...values,newFolder]); // Agregar la nueva carpeta a la lista
+            console.log('Carpeta creada:', newFolder);
+          },
+          error: (err) => {
+            console.error('Error al crear carpeta:', err);
+          },
+        });
+      }
+    });
+  }
+
+  deleteFolder(id: string, event: Event) {
+    event.stopPropagation();
+    this.archiveService.deleteFolder(id).subscribe({
+      next: () => {
+        this.folders.set( this.folders().filter((folder) => folder._id !== id));
+        console.log('Carpeta eliminada:', id);
+      },
+      error: (err) => {
+        console.error('Error al eliminar carpeta:', err);
+      },
+    });
   }
 }
