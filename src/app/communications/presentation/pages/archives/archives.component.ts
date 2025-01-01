638d5828d2c14d4d21c3bbf3f6db2a6967f1fd09
Gustavo Flores <gustavfs2003@gmail.com>
2025-01-01 16:15:45 -0400

    estructura de folders mejorada

--- src/app/communications/presentation/pages/archives/archives.component.ts
+++ src/app/communications/presentation/pages/archives/archives.component.ts
@@ -1,65 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Location } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ArchiveDialogComponent } from './archive-dialog/archive-dialog.component';
-import { ArchiveService } from '../../services/archive.service';
 
 @Component({
   selector: 'app-archives',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule,CommonModule],
+  imports: [MatToolbarModule,MatButtonModule,MatIconModule],
   templateUrl: './archives.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ArchivesComponent { 
-  folders = signal<any[]>([]);
-
-  constructor(private dialog: MatDialog,private archiveService: ArchiveService) {
-    this.loadFolders();
-
-  }
-
-  loadFolders() {
-    this.archiveService.getFolders().subscribe({
-      next: (data) => {
-        this.folders.set(data);
-      },
-      error: (err) => {
-        console.error('Error al cargar carpetas:', err);
-      },
-    });
-  }
-
-  openCreateFolderDialog() {
-    const dialogRef = this.dialog.open(ArchiveDialogComponent);
-
-    dialogRef.afterClosed().subscribe((folderName) => {
-      if (folderName) {
-        this.archiveService.createFolder(folderName).subscribe({
-          next: (newFolder) => {
-            this.folders.update((values)=>[...values,newFolder]); // Agregar la nueva carpeta a la lista
-            console.log('Carpeta creada:', newFolder);
-          },
-          error: (err) => {
-            console.error('Error al crear carpeta:', err);
-          },
-        });
-      }
-    });
-  }
-
-  deleteFolder(id: string, event: Event) {
-    event.stopPropagation();
-    this.archiveService.deleteFolder(id).subscribe({
-      next: () => {
-        this.folders.set( this.folders().filter((folder) => folder._id !== id));
-        console.log('Carpeta eliminada:', id);
-      },
-      error: (err) => {
-        console.error('Error al eliminar carpeta:', err);
-      },
-    });
+export default class ArchivesComponent {
+  private location = inject(Location);
+  
+  back() {
+    this.location.back();
   }
 }
---
+++ src/app/communications/presentation/pages/folders/folders.component.ts
@@ -0,0 +1,92 @@
+import { CommonModule, ViewportScroller } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, inject, Signal, signal, viewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FolderDialogComponent } from './folder-dialog/folder-dialog.component';
+import { ArchiveService } from '../../services/archive.service';
+import { AlertService } from '../../../../shared';
+import { Router, RouterModule, Scroll } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
+
+@Component({
+  selector: 'app-folders',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule,RouterModule],
+  templateUrl: './folders.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class FoldersComponent {
+  folders = signal<any[]>([]);
+  private alertService = inject(AlertService);
+  viewportScroller = inject(ViewportScroller);
+  scrollingRef = viewChild<HTMLElement>('scrolling');
+
+  constructor(private dialog: MatDialog, private archiveService: ArchiveService) {
+    this.loadFolders();
+    const scrollingPosition: Signal<[number, number] | undefined> = toSignal(
+      inject(Router).events.pipe(
+        filter((event): event is Scroll => event instanceof Scroll),
+        map((event: Scroll) => event.position || [0, 0]),
+      ),
+    );
+
+    effect(() => {
+      if (this.scrollingRef() && scrollingPosition()) {
+        this.viewportScroller.scrollToPosition(scrollingPosition()!);
+      }
+    });
+  }
+
+  loadFolders() {
+    this.archiveService.getFolders().subscribe({
+      next: (data) => {
+        this.folders.set(data);
+      },
+      error: (err) => {
+        console.error('Error al cargar carpetas:', err);
+      },
+    });
+  }
+
+  openCreateFolderDialog() {
+    const dialogRef = this.dialog.open(FolderDialogComponent);
+
+    dialogRef.afterClosed().subscribe((folderName) => {
+      if (folderName) {
+        this.archiveService.createFolder(folderName).subscribe({
+          next: (newFolder) => {
+            this.folders.update((values) => [...values, newFolder]); // Agregar la nueva carpeta a la lista
+            console.log('Carpeta creada:', newFolder);
+          },
+          error: (err) => {
+            console.error('Error al crear carpeta:', err);
+          },
+        });
+      }
+    });
+  }
+
+  deleteFolder(id: string, event: Event) {
+    event.stopPropagation();
+    this.alertService
+      .confirmDialog({
+        title: 'Eliminar carpeta?',
+        description:
+          'Se borrara la carpeta seleccionada',
+      }).subscribe((result) => {
+        if (!result) return;
+
+        this.archiveService.deleteFolder(id).subscribe({
+          next: () => {
+            this.folders.set(this.folders().filter((folder) => folder._id !== id));
+            console.log('Carpeta eliminada:', id);
+          },
+          error: (err) => {
+            console.error('Error al eliminar carpeta:', err);
+          },
+        });
+      });
+  }
+}
