e0e4fa588857ea92bdc950268966248ea88c9553
josel <jose@gmail.com>
2025-04-07 18:10:43 -0400

    correcion pipe duracion

--- src/app/communications/presentation/pipes/humanize-duration.pipe.ts
+++ src/app/communications/presentation/pipes/humanize-duration.pipe.ts
@@ -1,11 +1,10 @@
 import { Pipe, type PipeTransform } from '@angular/core';
 import { humanize } from '../../../helpers';
 
-@Pipe({
-  name: 'humanizeDuration',
-})
+@Pipe({ name: 'humanizeDuration' })
 export class HumanizeDurationPipe implements PipeTransform {
   transform(miliseconds: number): string {
-    return humanize(miliseconds);
+    const days = Math.floor(miliseconds / (1000 * 60 * 60 * 24));
+    return days > 0 ? humanize(miliseconds) : 'Expira hoy';
   }
 }
